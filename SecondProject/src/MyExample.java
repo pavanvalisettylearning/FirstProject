0549456cb8389dd1fcae5cac362723564d528458
Pavan <pavanvalisetty@gmail.com>
2020-10-03 13:12:00 +0530

    Added more condition- even or odd number logic

--- SecondProject/src/MyExample.java
+++ SecondProject/src/MyExample.java
@@ -7,6 +7,12 @@ public class MyExample {
 
 		System.out.println("First commit");
 		System.out.println("First commit 2 added more condition");
+		int a = 10;
+		if( a % 2 == 0) {
+			System.out.println(" even number");
+		}else {
+			System.out.println(" odd number ");
+		}
 		
 	}
 
